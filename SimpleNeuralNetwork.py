406b0e188d932307acacb5553affe61d3f6f3a02
Harshdeep Singh <44178466+HarshSingh16@users.noreply.github.com>
2018-12-26 04:26:05 -0500

    Update SimpleNeuralNetwork.py

--- SimpleNeuralNetwork.py
+++ SimpleNeuralNetwork.py
@@ -1,11 +1,33 @@
 #Simple Neural Network using Keras
 
+
 import keras
 from keras.models import Sequential
 from keras.layers import *
 
+
+#Building the Model
 model=k.models.Sequential()
 model.add(Dense(3,input_dim=2))#Input Layer
 model.add(Dense(3))
 model.add(Dense(1))#Output Layer
 model.compile(loss="mean_squared_error",optimizer="adam")
+
+
+#Training the model
+model.fit(training_data,expected_output)
+
+
+#Testing Phase
+error_rate=model.evaluate(testing_data,expected_output)
+
+
+#Saving the model to a file
+model.save("trained_model.h5")
+
+
+#Evaluating the model to make predictions
+model=keras.models.load_model("trained_model.h5")
+
+#Predictions
+Predictions=model.predict(new_Data)
