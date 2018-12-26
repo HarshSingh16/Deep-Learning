c47d08358ddb2d6e5208b7248895a01302b78468
Harshdeep Singh <44178466+HarshSingh16@users.noreply.github.com>
2018-12-26 09:52:39 -0500

    Update and rename cifar10.py to ImageRecognition.py

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -36,13 +36,24 @@ model = Sequential()
 
 model.add(Conv2D(32, (3, 3), padding="same", activation="relu", input_shape=(32, 32, 3)))
 model.add(Conv2D(32, (3, 3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2,2))) #Add maxpooling after a batch of Conv. Layer
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
 model.add(Conv2D(64, (3, 3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))  #Add maxpooling after a batch of Conv. Layer
-
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
 model.add(Flatten())
+
+model.add(Dense(512, activation="relu"))
+model.add(Dropout(0.50))
+model.add(Dense(10, activation="softmax"))
+
+
+# Print a summary of the model
+model.summary()
+
+
 
 model.add(Dense(512, activation="relu"))
 model.add(Dense(10, activation="softmax"))
