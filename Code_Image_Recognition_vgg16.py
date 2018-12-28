63d082e873d995457ea2b0cc6da73c106b8a5266
Harshdeep Singh <44178466+HarshSingh16@users.noreply.github.com>
2018-12-28 06:04:06 -0500

    ImageRecognition in Keras
    This code will do the following-
    1. Identify the object class in the image
    2. Import multiple images from directory to Python
    3. Resize, Reshape the dimensions so that they are for the model

--- Code_Image_Recognition_vgg16.py
+++ Code_Image_Recognition_vgg16.py
@@ -1,38 +1,29 @@
-# Load the json file that contains the model's structure
-#f = Path("model_structure.json")
-#model_structure = f.read_text()
+#Import the necessay packages
+
 import numpy as np
 from keras.preprocessing import image
 from keras.applications import vgg16
-# Recreate the Keras model object from the json data
-#model = model_from_json(model_structure)
-model = vgg16.VGG16()
-# Re-load the model's trained weights
-#model.load_weights("model_weights1.h5")
-
-
-# import the necessary packages
 import cv2
 import os, os.path
+
+#Load the vgg model from keras
+model = vgg16.VGG16()
+
  
-#debug info OpenCV version
-print ("OpenCV version: " + cv2.__version__)
- 
-#image path and valid extensions
+#image path and valid extensions- Setting the directory where we have stored the images
 imageDir = "/Users/harshsingh/Downloads/Ex_Files_Deep_Learning_Image_Recog/Exercise Files/Ch04/untitled folder" #specify your path here
 image_path_list = []
 valid_image_extensions = [".jpg", ".jpeg", ".png", ".tif", ".tiff"] #specify your vald extensions here
 valid_image_extensions = [item.lower() for item in valid_image_extensions]
  
-#create a list all files in directory and
-#append files with a vaild extention to image_path_list
+#create a list all files in directory and append files with a vaild extention to image_path_list
 for file in os.listdir(imageDir):
     extension = os.path.splitext(file)[1]
     if extension.lower() not in valid_image_extensions:
         continue
     image_path_list.append(os.path.join(imageDir, file))
  
-#loop through image_path_list to open each image
+#loop through image_path_list to open each image and subsequently throw in the predictions
 for imagePath in image_path_list:
     image = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
     width = 224
@@ -49,30 +40,3 @@ for imagePath in image_path_list:
     plt.imshow(image)
     # Show the plot on the screen
     plt.show()
-
-
-
-
-
-#for imagePath in image_path_list:
- #   image = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
-  #  width = 224
-  #  height = 224
-  #  dim = (width, height)
-  #  resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-  # list_of_images = np.expand_dims(resized, axis=0)
-  #  results = model.predict(list_of_images)
-  #  print(results)
-  #  single_result = results[0]
-  #  most_likely_class_index = int(np.argmax(single_result))
-  #  class_likelihood = single_result[most_likely_class_index]
-  #  class_label = class_labels[most_likely_class_index]
-  #  print("This is image is a {} - Likelihood: {:2f}".format(class_label, class_likelihood))
-  #  plt.imshow(image)
-    # Show the plot on the screen
-  #  plt.show()
-
-
-
-
-
